97caced64153e0064b6949ac038f4a863755f9bf
swen2014 <steven2hddx@gmail.com>
2015-01-27 18:58:32 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.m
+++ GameOfLife.spritebuilder/Creature.m
@@ -9,6 +9,17 @@
 #import "Creature.h"
 
 @implementation Creature
+- (instancetype)initCreature {
+    // since we made Creature inherit from CCSprite, 'super' below refers to CCSprite
+    self = [super initWithImageNamed:@"GameOfLifeAssets/Assets/bubble.png"];
+    
+    if (self) {
+        self.isAlive = NO;
+    }
+    
+    return self;
+}
+
 - (void)setIsAlive:(BOOL)newState {
     //when you create an @property as we did in the .h, an instance variable with a leading underscore is automatically created for you
     _isAlive = newState;
