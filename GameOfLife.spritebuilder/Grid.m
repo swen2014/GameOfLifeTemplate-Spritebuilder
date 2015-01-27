d2132dd06b047ba4ff905f4221801fb9a6c5ddaa
swen2014 <steven2hddx@gmail.com>
2015-01-27 18:59:53 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -66,23 +66,23 @@ static const int GRID_COLUMNS = 10;
     }
 }
 
-//- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
-//{
-//    //get the x,y coordinates of the touch
-//    CGPoint touchLocation = [touch locationInNode:self];
-//    
-//    //get the Creature at that location
-//    Creature *creature = [self creatureForTouchPosition:touchLocation];
-//    
+- (void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
+{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the Creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
 //    //invert it's state - kill it if it's alive, bring it to life if it's dead.
 //    creature.isAlive = !creature.isAlive;
-//}
-//- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
-//{
-//    //get the row and column that was touched, return the Creature inside the corresponding cell
-//    row = touchPosition.y/_cellHeight;
-//    column = touchPosition.x/_cellWidth;
-//    return _gridArray[row][column];
-//}
+}
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    row = touchPosition.y/_cellHeight;
+    column = touchPosition.x/_cellWidth;
+    return _gridArray[row][column];
+}
 
 @end
