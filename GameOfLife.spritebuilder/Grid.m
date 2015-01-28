46b8d01970972bd61040378d715e4d12d79c16d5
swen2014 <steven2hddx@gmail.com>
2015-01-27 19:01:14 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -74,8 +74,8 @@ static const int GRID_COLUMNS = 10;
     //get the Creature at that location
     Creature *creature = [self creatureForTouchPosition:touchLocation];
     
-//    //invert it's state - kill it if it's alive, bring it to life if it's dead.
-//    creature.isAlive = !creature.isAlive;
+    //invert it's state - kill it if it's alive, bring it to life if it's dead.
+    creature.isAlive = !creature.isAlive;
 }
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
